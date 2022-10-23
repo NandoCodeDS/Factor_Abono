24a220c37aca1534db154e64ad51129dab33319c
NandoCodeDS <99371351+NandoCodeDS@users.noreply.github.com>
2022-10-23 03:30:05 -0500

    Final de redes sociales con botón flotante

--- app/src/main/java/com/pm/factor_abono/DashboardActivity.java
+++ app/src/main/java/com/pm/factor_abono/DashboardActivity.java
@@ -6,17 +6,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.material.navigation.NavigationView;
 import com.pm.factor_abono.databinding.ActivityDashboardBinding;
 
 public class DashboardActivity extends DrawerBaseActivity {
 
     ActivityDashboardBinding activityDashboardBinding;
+    FloatingActionMenu actionMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,23 @@ public class DashboardActivity extends DrawerBaseActivity {
         activityDashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(activityDashboardBinding.getRoot());
         allocateActivityTittle("Dashboard");
+
+        actionMenu = (FloatingActionMenu) findViewById(R.id.menuflotante);
+        actionMenu.setClosedOnTouchOutside(true);
     }
 
 
+    public void fbkIntent(View view) {
+        String url = "https:/www.facebook.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    public void twtIntent(View view) {
+        String url = "https:/www.twitter.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
 }
