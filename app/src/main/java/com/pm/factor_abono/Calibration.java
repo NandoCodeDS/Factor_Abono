0b112981ffbbd0cc249674a6d99d2309cbd1e154
NandoCodeDS <99371351+NandoCodeDS@users.noreply.github.com>
2022-10-26 23:46:45 -0500

    Creacion de vista calculo de factor fase 1: Vista general y Spinners

---
+++ app/src/main/java/com/pm/factor_abono/Calibration.java
@@ -0,0 +1,36 @@
+package com.pm.factor_abono;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Calibration extends AppCompatActivity {
+
+    private Spinner spProducto;
+    private Spinner spEstado;
+    private Spinner spAbonadora;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calibration);
+
+        spProducto = (Spinner) findViewById(R.id.spProducto);
+        spEstado = (Spinner) findViewById(R.id.spEstado);
+        spAbonadora = (Spinner) findViewById(R.id.spAbonadora);
+
+        String[] opProducto ={"Urea","Mezcla"};
+        String[] opEstado ={"Plantilla","Soca"};
+        String[] opAbonadora ={"119222","121111","121112","121113"};
+
+        ArrayAdapter<String> prodcutos = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opProducto);
+        spProducto.setAdapter(prodcutos);
+        ArrayAdapter<String> estados = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opEstado);
+        spProducto.setAdapter(estados);
+        ArrayAdapter<String> abonadoras = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opAbonadora);
+        spProducto.setAdapter(abonadoras);
+    }
+}
